dabd56d6942f05946b1995ba992490632048270c
DishaGowda30 <dishagowda094@gmail.com>
2026-01-01 20:33:18 +0530

    Add files via upload

---
+++ pyeg2.py
@@ -0,0 +1,8 @@
+name = input("Enter your name : ")
+age = int(input("Enter your age: "))
+course = input("Enter your course: ")
+print(f"The name is {name}")
+print(f"The age is {age}")
+print(f"The course is {course}")
+age += 5
+print(f"The age after updation is {age}")
