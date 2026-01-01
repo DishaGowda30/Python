181189868f286212c69968c411ec275c4807e173
DishaGowda30 <dishagowda094@gmail.com>
2026-01-01 20:42:04 +0530

    Add files via upload

---
+++ pyeg4.py
@@ -0,0 +1,10 @@
+a=10
+b=3.14
+c="RPAFGC"
+d=True
+e=6+3.14j
+print("The value and data type of a is :",id(a))
+print("The value and data type of b is :",id(b))
+print("The value and data type of c is :",id(c))
+print("The value and data type of d is :",id(d))
+print("The value and data type of e is :",id(e))
